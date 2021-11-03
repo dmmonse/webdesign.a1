8fbe12882c87adc08822c0cfb57f9e201b2e5286
monse <78182277+dmmonse@users.noreply.github.com>
2021-11-03 13:01:59 -0500

    .js
    tag misspelled check

--- activepage.js
+++ activepage.js
@@ -1,6 +1,6 @@
 const activepage = window.location.pathname;
 const navlinks= document.querySelectorAll ('nav a ').
-foreach(link => {
+forEach(link => {
     if(link.href.includes('${activepage}' )){
 link.classlist.add('active');
     }
